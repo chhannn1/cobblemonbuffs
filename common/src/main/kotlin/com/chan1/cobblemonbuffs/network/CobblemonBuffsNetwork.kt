6394f86d41ca506a027b90d7b4a40e124874151d
chhannn1 <257209674+chhannn1@users.noreply.github.com>
2026-03-03 21:15:56 -0700

    fix p2

--- common/src/main/kotlin/com/chan1/cobblemonbuffs/network/CobblemonBuffsNetwork.kt
+++ common/src/main/kotlin/com/chan1/cobblemonbuffs/network/CobblemonBuffsNetwork.kt
@@ -1,8 +1,11 @@
 package com.chan1.cobblemonbuffs.network
 
 import com.chan1.cobblemonbuffs.CobblemonBuffs
+import dev.architectury.networking.NetworkManager
 import dev.architectury.networking.simple.MessageType
 import dev.architectury.networking.simple.SimpleNetworkManager
+import dev.architectury.utils.Env
+import dev.architectury.utils.EnvExecutor
 import net.minecraft.server.level.ServerPlayer
 
 object CobblemonBuffsNetwork {
@@ -17,6 +20,13 @@ object CobblemonBuffsNetwork {
     fun init() {
         AURA_SYNC_TYPE = NETWORK.registerS2C("aura_sync", ::AuraSyncPacket)
         CONFIG_SYNC_TYPE = NETWORK.registerS2C("config_sync", ::ConfigSyncPacket)
+
+        EnvExecutor.runInEnv(Env.SERVER) {
+            Runnable {
+                NetworkManager.registerS2CPayloadType(AURA_SYNC_TYPE.id)
+                NetworkManager.registerS2CPayloadType(CONFIG_SYNC_TYPE.id)
+            }
+        }
     }
 
     fun sendAuraSync(player: ServerPlayer, packet: AuraSyncPacket) {
